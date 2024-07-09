3094260c6424a8a6df383ae28e0da113e58ca005
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-07-09 11:58:42 +0800

    Finishn L8

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const GStore = reactive({ flashMessage: '' }) // Create a global (reactive) object to share across multiple
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore) // Make this object available for components in our app to use (inject)
   .mount('#app')
